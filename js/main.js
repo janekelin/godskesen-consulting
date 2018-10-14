130abbc5ac9c39895074bb5dc09a12340515e93f
Iana Godskesen Kalinichenko <iana-git@outlook.com>
2018-10-14 22:30:14 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Adds tilt and glare to cards leading to subdomains. From tilt.js (https://github.com/gijsroge/tilt.js)
+(function ($) {
+
+  "use strict";
+
+  $(".card").tilt({
+    maxTilt: 16,
+    perspective: 1400,
+    easing: "cubic-bezier(.03,.98,.52,.99)",
+    speed: 1200,
+    glare: true,
+    maxGlare: 0.3,
+    scale: 1.05 });
+
+
+})(jQuery);
