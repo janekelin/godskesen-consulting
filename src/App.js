377bdf8138fc38bc21f6542e22d92d22ac6cf927
Iana Godskesen Kalinichenko <iana-git@outlook.com>
2019-08-28 12:50:19 +0200

    Make App functional component
    The generated content is static,
    so itis enough to pass it as props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import Card from './Card';
 import React from 'react';
-import flemmingPic from './images/build/flemming.JPG';
-import ianaPic from './images/build/iana.JPG';
-import Card from './Card.js';
+
+// Components
+function App(props) {
+  const info = props.copyrightInfo;
+  const cards = props.persons.map((item, key) => 
+    <Card 
+      key={key}
+      customLink={item.link}
+      pic={item.pic}
+      cardTitle={item.name}
+      cardSubtitle={item.subtitle}
+    />);
+  return (
+    <main>
+        <section className="content">
+          {cards}
+        </section>
+        <CopyrightNote 
+          customLink={info.colorPaletteLink} 
+          author={info.colorPaletteAuthor} 
+          license={info.colorPaletteLicense} 
+        />
+      </main>
+  );
+}
 
 function CopyrightNote(props) {
   return (
@@ -13,58 +36,6 @@ function CopyrightNote(props) {
         </p>
       </aside>
   );
-}
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      colorPaletteLink: "https://www.colourlovers.com/palette/4570274/05.05.18",
-      colorPaletteAuthor: "https://www.colourlovers.com/lover/JMG84",
-      colorPaletteLicense: "https://creativecommons.org/licenses/by-nc-sa/3.0/",
-      persons: [
-        {
-          link: "https://flemming.godskesen.consulting",
-          name: "Flemming Godskesen",
-          subtitle: "PowerShell Junkie",
-          pic: flemmingPic
-      },
-        {
-          link: "https://iana.godskesen.consulting",
-          name: "Iana Godskesen",
-          subtitle: "Webdev Enthusiast",
-          pic: ianaPic
-        }
-      ]
-      
-      
-    };
-  }
-
-  render() {
-    const cards = this.state.persons.map((item, key) => 
-    <Card 
-      key={key}
-      customLink={item.link}
-      pic={item.pic}
-      cardTitle={item.name}
-      cardSubtitle={item.subtitle}
-    />);
-    return (
-      <main>
-        <section className="content">
-          {cards}
-        </section>
-        <CopyrightNote 
-          customLink={this.state.colorPaletteLink} 
-          author={this.state.colorPaletteAuthor} 
-          license={this.state.colorPaletteLicense} 
-        />
-      </main>
-    );
-  }
-
-  
 }
 
 export default App;
