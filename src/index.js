05bab9cc8ce377bf167bfb32074da7cde0fbf600
Iana Godskesen Kalinichenko <iana-git@outlook.com>
2019-08-28 12:37:30 +0200

    Refactor index.js
    Put progressive enhancement in the block
    that runs after the DOM content is loaded;
    Cache DOM elements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-document.querySelectorAll(".backup").forEach(i => i.className = "visuallyhidden");
-ReactDOM.render(<App />, document.getElementById('root'));
+// Progressive enhancement -> hides text that is shown when JS is disabled.
+document.addEventListener("DOMContentLoaded", addProgressiveEnhancement);
+function addProgressiveEnhancement(){
+  const elementsToHide = document.querySelectorAll(".backup");
+  elementsToHide.forEach(el => el.className = "visuallyhidden");
+}
+
+// Rendering
+const root = document.getElementById('root')
+ReactDOM.render(<App />, root);
