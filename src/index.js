f9cefbcd160c8e352aba229ce484355549429a44
Iana Godskesen Kalinichenko <iana-git@outlook.com>
2019-08-28 12:27:22 +0200

    Remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 document.querySelectorAll(".backup").forEach(i => i.className = "visuallyhidden");
 ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
